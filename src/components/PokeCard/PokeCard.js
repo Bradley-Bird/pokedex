5632748482f3511fcdeada9ca9312a337a1657ee
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-17 14:49:53 -0700

    added more attributes to pokecard.

--- src/components/PokeCard/PokeCard.js
+++ src/components/PokeCard/PokeCard.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import './PokeCard.scss';
 import './PokeCard.css';
+import $ from 'jquery';
 
-export default function PokeCard({ pokemon, type_1, type_2, url_image }) {
+{
+  var x;
+  var $cards = $('.pokeCard');
+  var $style = $('.hover');
+
+  $cards
+    .on('mousemove touchmove', (e) => {
+      // normalise touch/mouse
+      var pos = [e.offsetX, e.offsetY];
+      e.preventDefault();
+      if (e.type === 'touchmove') {
+        pos = [e.touches[0].clientX, e.touches[0].clientY];
+      }
+      var $card = $(this);
+      // math for mouse position
+      var l = pos[0];
+      var t = pos[1];
+      var h = $card.height();
+      var w = $card.width();
+      var px = Math.abs(Math.floor((100 / w) * l) - 100);
+      var py = Math.abs(Math.floor((100 / h) * t) - 100);
+      var pa = 50 - px + (50 - py);
+      // math for gradient / background positions
+      var lp = 50 + (px - 50) / 1.5;
+      var tp = 50 + (py - 50) / 1.5;
+      var px_spark = 50 + (px - 50) / 7;
+      var py_spark = 50 + (py - 50) / 7;
+      var p_opc = 20 + Math.abs(pa) * 1.5;
+      var ty = ((tp - 50) / 2) * -1;
+      var tx = ((lp - 50) / 1.5) * 0.5;
+      // css to apply for active card
+      var grad_pos = `background-position: ${lp}% ${tp}%;`;
+      var sprk_pos = `background-position: ${px_spark}% ${py_spark}%;`;
+      var opc = `opacity: ${p_opc / 100};`;
+      var tf = `transform: rotateX(${ty}deg) rotateY(${tx}deg)`;
+      // need to use a <style> tag for psuedo elements
+      var style = `
+      .card:hover:before { ${grad_pos} }  /* gradient */
+      .card:hover:after { ${sprk_pos} ${opc} }   /* sparkles */ 
+    `;
+      // set / apply css class and style
+      $cards.removeClass('active');
+      $card.removeClass('animated');
+      $card.attr('style', tf);
+      $style.html(style);
+      if (e.type === 'touchmove') {
+        return false;
+      }
+      clearTimeout(x);
+    })
+    .on('mouseout touchend touchcancel', () => {
+      // remove css, apply custom animation on end
+      var $card = $(this);
+      $style.html('');
+      $card.removeAttr('style');
+      x = setTimeout(() => {
+        $card.addClass('animated');
+      }, 2500);
+    });
+}
+
+export default function PokeCard({ pokemon, type_1, type_2, url_image, attack, defense }) {
   return (
-    <div className="pokeCard animated">
+    <div className="pokeCard">
       <div className="ImgBox">
         <img className="img" src={url_image} />
-        <span>Name: {pokemon}</span>
       </div>
+      <span>{pokemon.toUpperCase()}</span>
       <div className="stats">
-        <p>Main Type: {type_1}</p>
-        <p>Secondary Type: {type_2}</p>
+        <p>Type 1: {type_1}</p>
+        <p>Type 2: {type_2}</p>
+        <p>Attack:{attack}</p>
+        <p>Defense:{defense}</p>
       </div>
     </div>
   );
