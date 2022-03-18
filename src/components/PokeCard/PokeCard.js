8f476f82823e4dde01577f6cb511b38411b7c146
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-17 18:31:33 -0700

    cards style almost working, animates on mouseover but doesnt change gradient with mouse position

--- src/components/PokeCard/PokeCard.js
+++ src/components/PokeCard/PokeCard.js
@@ -1,82 +1,72 @@
-import React from 'react';
+import { useState } from 'react';
 import './PokeCard.scss';
 import './PokeCard.css';
-import $ from 'jquery';
+import classNames from 'classnames';
 
-{
-  var x;
-  var $cards = $('.pokeCard');
-  var $style = $('.hover');
+export default function PokeCard({
+  pokemon,
+  type_1,
+  type_2,
+  url_image,
+  attack,
+  defense,
+  className,
+}) {
+  const [cssStyles, setCssStyles] = useState({});
+  const [beforeStyles, setBeforeStyles] = useState({});
+  const [afterStyles, setAfterStyles] = useState({});
+  const [animatedState, setAnimatedState] = useState(false);
 
-  $cards
-    .on('mousemove touchmove', (e) => {
-      // normalise touch/mouse
-      var pos = [e.offsetX, e.offsetY];
-      e.preventDefault();
-      if (e.type === 'touchmove') {
-        pos = [e.touches[0].clientX, e.touches[0].clientY];
-      }
-      var $card = $(this);
-      // math for mouse position
-      var l = pos[0];
-      var t = pos[1];
-      var h = $card.height();
-      var w = $card.width();
-      var px = Math.abs(Math.floor((100 / w) * l) - 100);
-      var py = Math.abs(Math.floor((100 / h) * t) - 100);
-      var pa = 50 - px + (50 - py);
-      // math for gradient / background positions
-      var lp = 50 + (px - 50) / 1.5;
-      var tp = 50 + (py - 50) / 1.5;
-      var px_spark = 50 + (px - 50) / 7;
-      var py_spark = 50 + (py - 50) / 7;
-      var p_opc = 20 + Math.abs(pa) * 1.5;
-      var ty = ((tp - 50) / 2) * -1;
-      var tx = ((lp - 50) / 1.5) * 0.5;
-      // css to apply for active card
-      var grad_pos = `background-position: ${lp}% ${tp}%;`;
-      var sprk_pos = `background-position: ${px_spark}% ${py_spark}%;`;
-      var opc = `opacity: ${p_opc / 100};`;
-      var tf = `transform: rotateX(${ty}deg) rotateY(${tx}deg)`;
-      // need to use a <style> tag for psuedo elements
-      var style = `
-      .card:hover:before { ${grad_pos} }  /* gradient */
-      .card:hover:after { ${sprk_pos} ${opc} }   /* sparkles */ 
-    `;
-      // set / apply css class and style
-      $cards.removeClass('active');
-      $card.removeClass('animated');
-      $card.attr('style', tf);
-      $style.html(style);
-      if (e.type === 'touchmove') {
-        return false;
-      }
-      clearTimeout(x);
-    })
-    .on('mouseout touchend touchcancel', () => {
-      // remove css, apply custom animation on end
-      var $card = $(this);
-      $style.html('');
-      $card.removeAttr('style');
-      x = setTimeout(() => {
-        $card.addClass('animated');
-      }, 2500);
-    });
-}
-
-export default function PokeCard({ pokemon, type_1, type_2, url_image, attack, defense }) {
+  const handleMove = (e) => {
+    e.preventDefault();
+    const pos = [e.nativeEvent.offsetX, e.nativeEvent.offsetY];
+    setAnimatedState(true);
+    // console.log(pos);
+    //math for mouse position
+    const l = pos[0];
+    const t = pos[1];
+    const h = e.target.clientHeight;
+    const w = e.target.clientWidth;
+    const px = Math.abs(Math.floor((100 / w) * l) - 100);
+    const py = Math.abs(Math.floor((100 / h) * t) - 100);
+    const pa = 50 - px + (50 - py);
+    //math for gradient/background position
+    const lp = 50 + (px - 50) / 1.5;
+    const tp = 50 + (py - 50) / 1.5;
+    const px_spark = 50 + (px - 50) / 7;
+    const py_spark = 50 + (py - 50) / 7;
+    const p_opc = 20 + Math.abs(pa) * 1.5;
+    const ty = ((tp - 50) / 2) * -1;
+    const tx = ((lp - 50) / 1.5) * 0.5;
+    //css to apply for active card
+    const grad_pos = { backgroundPosition: `${lp}% ${tp}%;` };
+    const sprk_pos = { backgroundPosition: `${px_spark}% ${py_spark}%;` };
+    const opc = { opacity: `${p_opc / 100};` };
+    const tf = { tansform: `rotateX(${ty}deg) rotateY(${tx}deg)` };
+    setBeforeStyles(`${grad_pos}`);
+    setAfterStyles(`${sprk_pos} ${opc}`);
+  };
   return (
-    <div className="pokeCard">
-      <div className="ImgBox">
-        <img className="img" src={url_image} />
+    <>
+      <div styles={beforeStyles}></div>
+      <div
+        className={classNames({ pokeCard: true, animated: animatedState })}
+        style={cssStyles}
+        onMouseMove={(e) => handleMove(e)}
+        onMouseLeave={() => setAnimatedState(false)}
+      >
+        <div className="ImgBox">
+          <img className="img" src={url_image} />
+        </div>
+        <span>{pokemon.toUpperCase()}</span>
+        <div className="stats">
+          <p>Type 1: {type_1}</p>
+          <p>Type 2: {type_2}</p>
+          <p>Attack:{attack}</p>
+          <p>Defense:{defense}</p>
+        </div>
       </div>
-      <span>{pokemon.toUpperCase()}</span>
-      <div className="stats">
-        <p>Type 1: {type_1}</p>
-        <p>Type 2: {type_2}</p>
-        <p>Attack:{attack}</p>
-        <p>Defense:{defense}</p>
-      </div>
-    </div>
+      <div styles={afterStyles}></div>
+    </>
   );
 }
